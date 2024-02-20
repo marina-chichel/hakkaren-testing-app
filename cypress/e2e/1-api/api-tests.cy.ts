2f19eff5ffa21c242b1d7ea9f1d9fd8f8ec017ac
Marina Chichel <marina.chichel@lastingdynamics.com>
2024-02-20 15:30:38 +0100

    feat: add resetTo endpoint test

--- cypress/e2e/1-api/api-tests.cy.ts
+++ cypress/e2e/1-api/api-tests.cy.ts
@@ -2,7 +2,7 @@ describe("Test Hakkaren API", () => {
   beforeEach(() => {
     cy.visit("http://localhost:5173");
     cy.get("#dbURI").type(
-      "mongodb://65cb8ff7c04edaf78fcf47dd_65d2491ea58ff498cf919814_user:81pNkHM%7D-1ts@167.235.55.35:30558/65cb8ff7c04edaf78fcf47dd_65d2491ea58ff498cf919814?authSource=admin"
+      "mongodb://65cb8ff7c04edaf78fcf47dd_65d4958a6efd64dc8283a927_user:kLIwm*%3ElDD%23b@167.235.55.35:30558/65cb8ff7c04edaf78fcf47dd_65d4958a6efd64dc8283a927?authSource=admin"
     );
 
     cy.contains("DB is connected").should("not.exist");
@@ -19,7 +19,7 @@ describe("Test Hakkaren API", () => {
 
   it("Check the EXECUTE endpoint", () => {
     cy.get("#url").type(
-      "https://api.dev.hakkaren.lastingdynamics.net/v1/inceptors/65d24924a58ff498cf919c4e/execute"
+      "https://api.dev.hakkaren.lastingdynamics.net/v1/inceptors/65d495c76efd64dc8283ad61/execute"
     );
     cy.contains("Execute").click();
     cy.get("#responce").should("not.have.value", "");
@@ -33,29 +33,33 @@ describe("Test Hakkaren API", () => {
   it("Check the RESET endpoint", () => {
     cy.get("li").should("have.length.greaterThan", 0);
 
-    cy.get("#url").type(
-      "https://api.dev.hakkaren.lastingdynamics.net/v1/projects/65d2491ea58ff498cf919814/databases/reset"
-    );
+    const resetURL =
+      "https://api.dev.hakkaren.lastingdynamics.net/v1/projects/65d4958a6efd64dc8283a927/databases/reset";
+    cy.get("#url").type(resetURL);
     cy.get("#method").select("PUT");
     // _________________________________________________
-    // cy.get("#payload").type(`{ "resetTo": "initial" }`, {
-    //   parseSpecialCharSequences: false,
-    // });
-    // cy.contains("Execute").click();
+    cy.get("#payload").type(`{ "resetTo": "initial" }`, {
+      parseSpecialCharSequences: false,
+    });
 
-    // cy.get("ol").then(($list) => {
-    //   const usersNumber = $list.children().length;
-    //   cy.contains("Get users").click();
-    //   cy.get("ol").children().should("not.have.length", usersNumber);
-    // });
+    cy.intercept(resetURL).as("execution");
+    cy.contains("Execute").click();
+    cy.wait("@execution");
+    cy.get("ol").then(($list) => {
+      const usersNumber = $list.children().length;
+      cy.contains("Get users").click();
+      cy.get("ol").children().should("not.have.length", usersNumber);
+    });
     // _________________________________________________
 
     cy.get("#payload").clear().type(`{ "resetTo": "empty" }`, {
       parseSpecialCharSequences: false,
     });
     cy.contains("Execute").click();
+    cy.wait("@execution");
 
     cy.contains("Get users").click();
+
     cy.get("ol").children().should("have.length", 0);
   });
 });
