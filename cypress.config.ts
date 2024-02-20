1d21aa7b71899d4de1c59ce56644bfdd9761578d
Marina Chichel <marina.chichel@lastingdynamics.com>
2024-02-20 16:46:12 +0100

    feat: settings

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,5 +6,7 @@ export default defineConfig({
       // implement node event listeners here
     },
     viewportHeight: 1024,
+    baseUrl: "http://localhost:5173",
+    video: true,
   },
 });
--- cypress/e2e/1-api/api-tests.cy.ts
+++ cypress/e2e/1-api/api-tests.cy.ts
@@ -1,9 +1,19 @@
+const dbURI =
+  "mongodb://65cb8ff7c04edaf78fcf47dd_65d4958a6efd64dc8283a927_user:kLIwm*%3ElDD%23b@167.235.55.35:30558/65cb8ff7c04edaf78fcf47dd_65d4958a6efd64dc8283a927?authSource=admin";
+
+const authToken =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NWNiOGZmN2MwNGVkYWY3OGZjZjQ3ZGQiLCJpYXQiOjE3MDgzNjM1MDQsImV4cCI6MTcwODQ0OTkwNH0.PdPb3knV0AnBNrK-OSGjLFj9OAJij_62j-BPSvE8frg";
+
+const resetURL =
+  "https://api.dev.hakkaren.lastingdynamics.net/v1/projects/65d4958a6efd64dc8283a927/databases/reset";
+
+const executeURL =
+  "https://api.dev.hakkaren.lastingdynamics.net/v1/inceptors/65d495c76efd64dc8283ad61/execute";
+
 describe("Test Hakkaren API", () => {
   beforeEach(() => {
-    cy.visit("http://localhost:5173");
-    cy.get("#dbURI").type(
-      "mongodb://65cb8ff7c04edaf78fcf47dd_65d4958a6efd64dc8283a927_user:kLIwm*%3ElDD%23b@167.235.55.35:30558/65cb8ff7c04edaf78fcf47dd_65d4958a6efd64dc8283a927?authSource=admin"
-    );
+    cy.visit("/");
+    cy.get("#dbURI").type(dbURI);
 
     cy.contains("DB is connected").should("not.exist");
     cy.contains("Connect").click();
@@ -12,16 +22,16 @@ describe("Test Hakkaren API", () => {
     cy.get("li").should("have.length", 0);
     cy.contains("Get users").click();
 
-    cy.get("#auth-token").type(
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NWNiOGZmN2MwNGVkYWY3OGZjZjQ3ZGQiLCJpYXQiOjE3MDgzNjM1MDQsImV4cCI6MTcwODQ0OTkwNH0.PdPb3knV0AnBNrK-OSGjLFj9OAJij_62j-BPSvE8frg"
-    );
+    cy.get("#auth-token").type(authToken);
   });
 
   it("Check the EXECUTE endpoint", () => {
-    cy.get("#url").type(
-      "https://api.dev.hakkaren.lastingdynamics.net/v1/inceptors/65d495c76efd64dc8283ad61/execute"
-    );
+    cy.get("#url").type(executeURL);
+
+    cy.intercept(executeURL).as("execution");
     cy.contains("Execute").click();
+    cy.wait("@execution");
+
     cy.get("#responce").should("not.have.value", "");
     cy.get("ol").then(($list) => {
       const usersNumber = $list.children().length;
@@ -33,8 +43,6 @@ describe("Test Hakkaren API", () => {
   it("Check the RESET endpoint", () => {
     cy.get("li").should("have.length.greaterThan", 0);
 
-    const resetURL =
-      "https://api.dev.hakkaren.lastingdynamics.net/v1/projects/65d4958a6efd64dc8283a927/databases/reset";
     cy.get("#url").type(resetURL);
     cy.get("#method").select("PUT");
     // _________________________________________________
