114b3b03b86615cbffd9f5f6349ea82b524ae19a
Marina Chichel <marina.chichel@lastingdynamics.com>
2024-02-21 19:09:18 +0100

    feat: add tests

--- src/components/hooks/useAPI.tsx
+++ src/components/hooks/useAPI.tsx
@@ -1,4 +1,10 @@
 import { useEffect, useState } from "react";
+import {
+  connectionString,
+  authToken,
+  resetURL,
+  executeURL,
+} from "../../../api-settings";
 
 type UserResp = {
   _id: string;
@@ -16,18 +22,6 @@ export type User = {
   team: string;
   date: string;
 };
-
-const connectionString =
-  "mongodb://65cb8ff7c04edaf78fcf47dd_65d4958a6efd64dc8283a927_user:kLIwm*%3ElDD%23b@167.235.55.35:30558/65cb8ff7c04edaf78fcf47dd_65d4958a6efd64dc8283a927?authSource=admin";
-
-const authToken =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NWNiOGZmN2MwNGVkYWY3OGZjZjQ3ZGQiLCJpYXQiOjE3MDg0NTEyMTcsImV4cCI6MTcwODUzNzYxN30._wAxNMTBmvFfMaoVoPGs-Bl2RuVhJbFQktLCCnO5mwk";
-
-const executeURL =
-  "https://api.dev.hakkaren.lastingdynamics.net/v1/inceptors/65d4e7c86efd64dc82843d14/execute";
-
-const resetURL =
-  "https://api.dev.hakkaren.lastingdynamics.net/v1/projects/65d4958a6efd64dc8283a927/databases/reset";
 
 const useAPI = () => {
   const [isFetching, setIsFetching] = useState(false);
