f66be399f0168075404d3a6fb1579206b1cc89da
Marina Chichel <marina.chichel@lastingdynamics.com>
2024-03-15 16:01:47 +0100

    change project

--- api-settings.ts
+++ api-settings.ts
@@ -1,11 +1,11 @@
 export const connectionString =
-  "mongodb://65e1e37412446e5d764f1db7_65e9ae23421b6bafb651e6f9_user:fibqaFqPSjS-@a1d82b4a7937a4fa6883a0a39d1b7deb-1860942038.us-east-2.elb.amazonaws.com:27017/65e1e37412446e5d764f1db7_65e9ae23421b6bafb651e6f9?authSource=admin";
+  "mongodb://65e1e37412446e5d764f1db7_65eae52b7ffdb54a2cf68756_user:%7D4u6.qnW*-K%3C@a1d82b4a7937a4fa6883a0a39d1b7deb-1860942038.us-east-2.elb.amazonaws.com:27017/65e1e37412446e5d764f1db7_65eae52b7ffdb54a2cf68756?authSource=admin";
 
 export const hakkarenURL = "https://api.dev.app.hakkaren.co/v1";
 // const hakkarenURL = "https://api.dev.hakkaren.lastingdynamics.net/v1";
-export const executeURL = `${hakkarenURL}/inceptors/65e9b0b4421b6bafb651ecf5/execute`;
+export const executeURL = `${hakkarenURL}/inceptors/65f4601bc37aed9030e5805c/execute`;
 
-export const resetURL = `${hakkarenURL}/projects/65e9ae23421b6bafb651e6f9/databases/reset`;
+export const resetURL = `${hakkarenURL}/projects/65eae52b7ffdb54a2cf68756/databases/reset`;
 
 export const loginURL = `${hakkarenURL}/auth/login/`;
 
